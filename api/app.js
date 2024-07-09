1973760bc8e9fb4c3c5c145350e626db28d8e8eb
Talha Ansari <talhaansari1606@gmail.com>
2024-07-10 02:07:29 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
-    credentials: true 
+    credentials: false 
 }))
 app.use(cookieParser())
 app.use(express.json())
